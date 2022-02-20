27c0542284dab3c57dd8482820b463c80f54374d
Maximilian Walter <maximilian.walter@kit.edu>
2022-02-20 10:44:17 +0100

    removed unecessary prolog dependency

--- bundles/edu.kit.kastel.dsis.fluidtrust.casestudy.pcs.application/src/edu/kit/kastel/dsis/fluidtrust/casestudy/pcs/application/internal/Activator.java
+++ bundles/edu.kit.kastel.dsis.fluidtrust.casestudy.pcs.application/src/edu/kit/kastel/dsis/fluidtrust/casestudy/pcs/application/internal/Activator.java
@@ -1,11 +1,9 @@
 package edu.kit.kastel.dsis.fluidtrust.casestudy.pcs.application.internal;
 
-import java.util.Optional;
-
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
-import org.prolog4j.manager.IProverManager;
+
 
 public class Activator extends Plugin {
 
@@ -21,16 +19,16 @@ public class Activator extends Plugin {
         }
 
         protected S init() {
-            serviceReference = context.getServiceReference(serviceClass);
-            service = context.getService(serviceReference);
-            return service;
+            this.serviceReference = this.context.getServiceReference(this.serviceClass);
+            this.service = this.context.getService(this.serviceReference);
+            return this.service;
         }
 
         public S get() {
-            var localRef = service;
+            var localRef = this.service;
             if (localRef == null) {
                 synchronized (this) {
-                    localRef = service;
+                    localRef = this.service;
                     if (localRef == null) {
                         localRef = init();
                     }
@@ -41,32 +39,17 @@ public class Activator extends Plugin {
 
         public void uninit() {
             synchronized(this) {
-                if (serviceReference != null) {
-                    context.ungetService(serviceReference);
-                    serviceReference = null;
-                    service = null;                                    
+                if (this.serviceReference != null) {
+                    this.context.ungetService(this.serviceReference);
+                    this.serviceReference = null;
+                    this.service = null;
                 }
             }
         }
     }
 
     private static Activator instance;
-    private ServiceProvider<IProverManager> proverManager;
 
-    @Override
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        setInstance(this);
-        proverManager = new ServiceProvider<>(IProverManager.class, context);
-    }
-
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        proverManager.uninit();
-        proverManager = null;
-        setInstance(null);
-        super.stop(context);
-    }
 
     private static void setInstance(Activator instance) {
         Activator.instance = instance;
@@ -74,11 +57,5 @@ public class Activator extends Plugin {
 
     public static Activator getInstance() {
         return Activator.instance;
-    }
-
-    public IProverManager getProverManager() {
-        return Optional.ofNullable(proverManager)
-            .map(ServiceProvider::get)
-            .orElse(null);
     }
 }
